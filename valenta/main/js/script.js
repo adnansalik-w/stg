9900a7f296152fea6057e04ba9ccc8c513b46d25
Adnan Saliq <adnan.saliq@valentabpo.com>
2022-08-18 12:09:39 +0530

    made css and js changes

--- valenta/main/js/script.js
+++ valenta/main/js/script.js
@@ -1,17 +1,36 @@
-function togg(e) {
-    let id = e.getAttribute('id');
-    fun(id);
-}
+// let thumbWrapper;
+// document.addEventListener("DOMContentLoaded", () => {
+//   thumbWrapper = document.getElementsByClassName("rwc-embed__thumb-wrapper");
+//   console.log(thumbWrapper);
+// });
 
-function fun(id){
-    const elem = document.getElementsByClassName(id);
-    const v_elem = document.getElementsByClassName('cart-menu--shown')
-    
-    for (let v_el of v_elem){
-        v_el.classList.remove("cart-menu--shown");
-        v_el.classList.add("cart-menu--hidden");
-    }
-    for (let el of elem) {
-    el.classList.add("cart-menu--shown");
+
+
+// for (let index = 0; index < thumbWrapper.length; index++) {
+//   const element = thumbWrapper[index];
+//   let childNodes = element.childNodes;
+//   for (let i = 0; i < childNodes.length; i++) {
+//     let outHTML = childNodes[i].outerHTML;
+//     if (outHTML.includes("img")) {
+//       childNodes[i].addEventListener("click", () => {
+//         console.log(childNodes[i].outerHTML);
+//         outHTML = `<img src="./images/botold.png" alt="Chat logo">`;
+//         childNodes[i].outerHTML = outHTML;
+//         console.log(childNodes[i].outerHTML);
+//       });
+//     }
+//   }
+// }
+
+let rwcDiv = document.getElementById("rwc");
+rwcDiv.addEventListener("click", () => {
+  let thumbWrapper = document.getElementsByClassName(
+    "rwc-embed__thumb-wrapper"
+  );
+  for (let index = 0; index < thumbWrapper.length; index++) {
+    const element = thumbWrapper[index];
+    let childNodes = element.childNodes;
+    let outHTML = `<img src="https://files.orsolaas.api.onereach.ai/public/81b9e903-15fa-48dc-9a5e-59483738c4a5/Team_Khizar/Adnan/Icons/bot_img.png" alt="Chat logo">`;
+    childNodes[0].outerHTML = outHTML;
   }
-}
+});
